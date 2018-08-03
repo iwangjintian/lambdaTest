e8dcd2de678928d614164a18cf2b1b39958a9eef
iwangjintian <597493748@qq.com>
2018-08-03 13:53:35 -0400

    update

--- buildspec.sh
+++ buildspec.sh
@@ -6,8 +6,8 @@ do
 counter=$((counter+1))
 if [ "$counter" -gt 6 ]
 then
-  filelocation=$LINE
-  folders=$filelocation[1]
+  filelocation=($LINE)
+  folders=${filelocation[1]}
     #echo "${folders}"
     if [[ $folders = *"/"* ]]
     then
@@ -17,8 +17,8 @@ fi
 done < output.txt
 if [ -f folders.txt ]
 then
-  cat folders.txt
+  #cat folders.txt
   sort -u folders.txt >> unique_folders.txt
-  cat unique_folders.txt
+  #cat unique_folders.txt
   for FILE in `cat unique_folders.txt`;do cd $FILE;chmod +x deploy.sh;./deploy.sh;cd ..;done
 fi
