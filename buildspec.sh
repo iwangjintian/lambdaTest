15a06a5c12b4a8ffff1446d57efb7b123ddf0728
iwangjintian <597493748@qq.com>
2018-08-03 10:39:54 -0400

    update

---
+++ buildspec.sh
@@ -0,0 +1,16 @@
+git show --name-status | tee output.txt
+count=0
+while read LINE
+do
+counter=$((counter+1))
+if [[ "$counter" -gt 6 ]]; then
+  filelocation=($LINE)
+  folders=${filelocation[1]}
+    #echo "${folders}"
+    if [[ $folders = *"/"* ]]; then
+      echo "${folders%%/*}" >> folders.txt
+  fi
+fi
+done < output.txt
+sort -u folders.txt >> unique_folders.txt
+for FILE in `cat unique_folders.txt`;do cd $FILE;chmod +x deploy.sh;./deploy.sh;cd ..;done
